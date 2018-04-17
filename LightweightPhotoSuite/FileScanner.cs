609949e112aa240ffc5d56ed2b56a2b53c822052
Joachim Goebgen <Joachim.Goebgen@bwedu.de>
2018-04-17 21:05:42 +0200

    added class- and method-stubs

---
+++ LightweightPhotoSuite/FileScanner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightweightPhotoSuite
+{
+    class FileScanner
+    {
+        private List<string> pathsToScan;
+        private Dictionary<string, Photo> allScannedPhotos;
+
+        public FileScanner()
+        {
+            pathsToScan = new List<string>();
+            allScannedPhotos = new Dictionary<string, Photo>();
+        }
+
+        public FileScanner(DataManagement db)
+        {
+
+        }
+
+        private void startScan()
+        {
+
+        }
+    }
+}
---
+++ LightweightPhotoSuite/Photo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightweightPhotoSuite
+{
+    class Photo
+    {
+        private List<string> tags;
+        private string filePath;
+        private DateTime exposureDate;
+        private int width_px;
+        private int height_px;
+
+        public Photo(string filePath, DateTime exposureDate, int width_px, int height_px)
+        {
+            this.filePath = filePath;
+            this.exposureDate = exposureDate;
+            this.width_px = width_px;
+            this.height_px = height_px;
+        }
+
+        public bool addTag(string tag)
+        {
+            if (!tags.Contains(tag))
+            {
+                tags.Add(tag);
+                return true;
+            }
+
+            return false;
+        }
+
+        public List<string> getTags()
+        {
+            return tags.ToList(); // shallow copy
+        }
+
+        public string getFilePath()
+        {
+            return filePath;
+        }
+    }
+}
