b903d5ed386c5e8f42a42948b3d0c483daeed913
JoachimGoebgen <36620066+JoachimGoebgen@users.noreply.github.com>
2018-04-17 21:05:42 +0200

    added class- and method-stubs

---
+++ LightweightPhotoSuite/DataManagement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightweightPhotoSuite
+{
+    class DataManagement
+    {
+        
+        public DataManagement()
+        {
+
+        }
+
+        public DataManagement(string pathToDatabaseFile)
+        {
+
+        }
+
+        public void doAddTag(Photo photo)
+        {
+
+        }
+
+        public void doRemoveTag(Photo photo)
+        {
+
+        }
+
+        public void doBackup()
+        {
+
+        }
+
+        public void doNewPhotoScan()
+        {
+
+        }
+
+        public void doFullScan()
+        {
+
+        }
+
+    }
+}
