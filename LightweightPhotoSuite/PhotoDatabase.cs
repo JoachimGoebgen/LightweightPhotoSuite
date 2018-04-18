9182ca9316bb3bbaf4322180e027e99af1bea73e
JoachimGoebgen <36620066+JoachimGoebgen@users.noreply.github.com>
2018-04-18 17:14:55 +0200

    added more method-stubs and attributes

--- LightweightPhotoSuite/PhotoDatabase.cs
+++ LightweightPhotoSuite/PhotoDatabase.cs
@@ -8,6 +8,70 @@ namespace LightweightPhotoSuite
 {
     class PhotoDatabase
     {
-        private Dictionary<string, Photo> tagToPhotos;
+        object lockObj;
+
+        private List<Tag> allTags;
+        private List<Photo> allPhotos;
+        private Dictionary<Tag, List<Photo>> tagToPhoto;
+
+        public Tag[] getAllTagsCopy()
+        {
+            Tag[] allTagsCopy;
+
+            lock (lockObj)
+                allTagsCopy = allTags.ToArray();
+            
+            return allTagsCopy;
+        }
+
+        public Photo[] getAllPhotosCopy()
+        {
+            Photo[] allPhotosCopy;
+
+            lock (lockObj)
+                allPhotosCopy = allPhotos.ToArray();
+
+            return allPhotosCopy;
+        }
+
+        public void addPhoto(Photo photo)
+        {
+
+        }
+
+        public void addPhotos(IEnumerable<Photo> photos)
+        {
+
+        }
+
+        public void addPhotoWithTags(Photo photo, IEnumerable<Tag> tags)
+        {
+
+        }
+
+        public void addPhotosWithTags(IEnumerable<Photo> photos, IEnumerable<Tag> tags)
+        {
+
+        }
+
+        public void addTag(Photo photo, Tag tag)
+        {
+
+        }
+
+        public void addTags(Photo photo, IEnumerable<Tag> tags)
+        {
+
+        }
+
+        public void addTag(IEnumerable<Photo> photos, Tag tag)
+        {
+
+        }
+
+        public void addTags(IEnumerable<Photo> photos, IEnumerable<Tag> tags)
+        {
+
+        }
     }
 }
