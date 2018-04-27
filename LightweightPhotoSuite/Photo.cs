74b944cd8d3a4630d91690fba9bb041995f9fa64
JoachimGoebgen <36620066+JoachimGoebgen@users.noreply.github.com>
2018-04-27 23:09:10 +0200

    added PhotoStub and made Photo only reference the tag-list

--- LightweightPhotoSuite/Photo.cs
+++ LightweightPhotoSuite/Photo.cs
@@ -12,31 +12,38 @@ namespace LightweightPhotoSuite
         private string filePath;
         private DateTime exposureDate;
 
-        public Photo(string filePath, string exposureDate)
+        public Photo(PhotoStub stub, List<Tag> tagListRef)
         {
-            this.tags = new List<Tag>();
-            this.filePath = filePath;
-            Logger.log(exposureDate);
-            this.exposureDate = new DateTime();
-        }
-
-        /// <summary>
-        /// Must only be called by PhotoDatabase or we'll end up with inconsistent data!
-        /// </summary>
-        /// <param name="tag"></param>
-        public void addTag(Tag tag)
-        {
-            tags.Add(tag);
+            filePath = stub.filePath;
+            exposureDate = stub.exposureDate;
+            tags = tagListRef; // reference! Tags get changed in this list if changed in original list.
         }
 
         public Tag[] getTagsCopy()
         {
-            return tags.ToArray();
+            Tag[] ret;
+            lock (tags)
+            {
+                ret = tags.ToArray();
+            }
+            return ret;
         }
 
         public string getFilePath()
         {
             return filePath;
+        }
+
+        public override string ToString()
+        {
+            string str = filePath + '|';
+            lock (tags)
+            {
+                for (int i = 0; i < tags.Count; i++)
+                    str += tags[i].name + ',';
+            }
+            str += '|' + exposureDate.ToString();
+            return str;
         }
     }
 }
---
+++ LightweightPhotoSuite/PhotoStub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightweightPhotoSuite
+{
+    struct PhotoStub
+    {
+        public string filePath;
+        public DateTime exposureDate;
+
+        public PhotoStub(string filePath, DateTime exposureDate)
+        {
+            this.filePath = filePath;
+            this.exposureDate = exposureDate;
+        }
+    }
+}
